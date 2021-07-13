06b4376b9ed8aa745afe09e0de6a3f4d8a693f9b
sarojvrc <saroj.vrc@gmail.com>
2021-07-14 01:23:40 +0530

    July 14

--- ArrayMinNumber.java
+++ ArrayMinNumber.java
@@ -3,12 +3,13 @@ package dsa;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ArrayMaxNumber {
-	public static void main(String args[]) {
+public class ArrayMinNumber {
+
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the array size");
 		int size = sc.nextInt();
-		int max = 0;
+		int min = 0;
 		
 		int arr[] = new int[size];
 		System.out.println("Enter the array elements");
@@ -16,13 +17,14 @@ public class ArrayMaxNumber {
 			arr[i] = sc.nextInt();
 		}
 		System.out.println(Arrays.toString(arr));
+		min = arr[0];
 		
 		for(int i=0; i<arr.length; i++) {
-			
-			if(arr[i]>max) {
-				max = arr[i];
+			if(arr[i]<min) {
+				min = arr[i];
 			}
 		}
-		System.out.println("The max element is "+max);
+		System.out.println("The min element is "+min);
 	}
+
 }
