77cbfa1fc7d46accadbca4e57422b3e72711106c
sarojvrc <saroj.vrc@gmail.com>
2021-07-13 23:27:24 +0530

    July 13

--- ArraySort2.java
+++ ArraySort2.java
@@ -1,5 +1,31 @@
 package dsa;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArraySort2 {
-
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the size");
+		int size = sc.nextInt();
+		int arr[] = new int[size];
+		int temp = 0;
+		System.out.println("Enter elements");
+		for(int i=0; i<arr.length; i++) {
+			arr[i]=sc.nextInt();
+		}
+		System.out.println(Arrays.toString(arr));
+		
+		for(int i=0; i<arr.length; i++) {
+			for(int j= i+1; j<arr.length; j++) {
+				if(arr[i]>arr[j]) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		for(int i=0; i<arr.length;i++) {
+			System.out.print( arr[i]+ " ");
+		}
+	}
 }
