e995c5e8134f6b52d67a0297208874e4014ea836
sarojvrc <saroj.vrc@gmail.com>
2021-07-13 22:51:23 +0530

    July 13

---
+++ ArrayMinRepeate.java
@@ -0,0 +1,26 @@
+package dsa;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayMinRepeate {
+	public static void main(String args[]) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the array size");
+		int size = sc.nextInt();
+		int min = 0;
+		int arr[] = new int[size];
+		System.out.println("Enter the array elements");
+		for(int i=0; i<arr.length; i++) {
+			arr[i]=sc.nextInt();
+		}
+		System.out.println(Arrays.toString(arr));
+		min = arr[0];
+		for(int i=1; i<arr.length; i++) {
+			if(arr[i]<min) {
+				min = arr[i];
+			}
+		}
+		System.out.println("The small element is "+min);
+	}
+}
