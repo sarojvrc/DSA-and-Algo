3ad2086f0931d5b04233ef99f6fa2cab9cabdd38
sarojvrc <saroj.vrc@gmail.com>
2021-07-18 23:45:20 +0530

    July 18

---
+++ StringCompare.java
@@ -0,0 +1,25 @@
+package dsa;
+import java.util.Scanner;
+
+public class StringCompare {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 1st String");
+		String str1 = sc.nextLine();
+		System.out.println("Enter 2nd String");
+		String str2 = sc.nextLine();
+		
+		boolean compare = str1.equals(str2);
+		System.out.println("You inputted for String 1 is "+str1);
+		System.out.println("You inputted for String 2 is "+str2);
+		if(str1 == str2) {
+			System.out.println(str1 + " and "+str2+" both are equal");
+		}
+		if(compare ==  true) {
+			System.out.println("Compare returns true");
+		}else {
+			System.out.println("Compare returns false");
+		}
+	}
+
+}
