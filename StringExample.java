f6c433d4c7773d9b5e78a32ca51f1ec0a8a4d897
sarojvrc <saroj.vrc@gmail.com>
2021-07-18 23:26:10 +0530

    July 18

---
+++ StringExample.java
@@ -0,0 +1,12 @@
+package dsa;
+import java.util.Scanner;
+
+public class StringExample {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Type any String:");
+		String str = sc.nextLine();
+		System.out.println("You inputted "+str);
+	}
+
+}
