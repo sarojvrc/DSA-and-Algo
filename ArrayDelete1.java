f01dbfb25fa4b280b632ba5b3f8428294401193a
Saroj Kumar Behera <53892172+sarojvrc@users.noreply.github.com>
2021-07-31 23:53:10 +0530

    Array Delete

---
+++ ArrayDelete1.java
@@ -0,0 +1,35 @@
+package dsa;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayDelete1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the array size");
+		int size = sc.nextInt();
+		int arr[] = new int[size];
+		System.out.println("Enter array elements");
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		//to see our array
+		System.out.println("Array is "+Arrays.toString(arr));
+		System.out.println("Enter the index location to delete the array");
+		 int loc = sc.nextInt();
+		 
+		 for(int i=loc; i<size-1; i++) {
+			 arr[i] = arr[i+1];
+		 }
+		 size--;
+//		 for(int i=0; i<arr.length; i++) {
+//			
+//		 }
+		 
+		 System.out.println("New array is "+Arrays.toString(arr));
+
+	}
+
+}
