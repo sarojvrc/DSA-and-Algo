2a12d91d77df46ee71f1b3d280f84fe42ac01bf8
Saroj Kumar Behera <53892172+sarojvrc@users.noreply.github.com>
2021-08-01 12:29:01 +0530

    Sort

---
+++ ArraySortnew.java
@@ -0,0 +1,17 @@
+package dsa;
+import java.util.Arrays;
+
+public class ArraySortnew {
+
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+		int arr[] = {21, 5, 24, 58};
+		String arr2[] = {"Saroj", "Madan", "New"};
+		Arrays.sort(arr);
+		Arrays.sort(arr2);
+		System.out.println("Sort of int arr " + Arrays.toString(arr));
+		System.out.println("Sort of string array "+Arrays.toString(arr2));
+
+	}
+
+}
