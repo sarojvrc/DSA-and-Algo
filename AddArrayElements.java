64566997679b8c44beaade50805c9059696b669e
Saroj Kumar <53892172+sarojvrc@users.noreply.github.com>
2021-08-04 23:22:02 +0530

    Add array elements

---
+++ AddArrayElements.java
@@ -0,0 +1,27 @@
+package practice;
+
+import java.util.Arrays;
+
+public class AddArrayElements {
+	
+	//function to add elements
+	static void AddElement(int arr[]) {
+		int sum = 0;
+		for(int i=0; i<arr.length; i++) {
+			sum = sum + arr[i];
+		}
+		System.out.println("The sum of all elements is "+sum);
+	}
+
+	//driver code
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {2,2,1};
+		System.out.println("Array is "+Arrays.toString(arr));
+		
+		//call the add method
+		AddArrayElements.AddElement(arr);
+
+	}
+
+}
