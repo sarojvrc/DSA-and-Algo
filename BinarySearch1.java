00d707f88bb9d51b0441639544936d0d7dabd2a6
Saroj Kumar <53892172+sarojvrc@users.noreply.github.com>
2021-08-17 15:47:25 +0530

    find the element from an array using binary search

---
+++ BinarySearch1.java
@@ -0,0 +1,48 @@
+package dsa;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearch1 {
+	
+	//function for binarysearch
+	static void binarysearch(int arr[], int element) {
+		int low = 0;
+		int high = arr.length-1;
+		int mid = (low+high)/2;
+		int count = 0;
+		while(low<=high) {
+			if(arr[mid] == element) {
+				System.out.println("Element "+element+" found at "+mid+"th position");
+				count++;
+				break;
+			}else if(arr[mid]<element) {
+				low = mid+1;
+			}else {
+				high = mid-1;
+			}
+			mid = (low+high)/2;
+		}
+		if(count == 0) {
+			System.out.println("Element not found");
+		}
+	}
+	//driver code
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the array size");
+		int size = sc.nextInt();
+		System.out.println("Enter the array elements in a sorted format");
+		int arr[] = new int[size];
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println("Array is "+Arrays.toString(arr));
+		System.out.println("Enter the element to search");
+		int element = sc.nextInt();
+		
+		//now call the searchfunction
+		binarysearch(arr, element);
+	}
+
+}
