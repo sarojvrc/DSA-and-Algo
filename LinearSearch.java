04b18715bfbd4da11e8213079b342d91cb1f4c54
Saroj Kumar <53892172+sarojvrc@users.noreply.github.com>
2021-08-24 12:44:33 +0530

    Linear Search

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,42 +1,40 @@
-package linkedlist;
+package practice;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class LinearSearch {
 	
-	//method for linearSearch
+	//function for linearSearch
 	static void linearSearch(int arr[], int element) {
 		int count = 0;
 		for(int i=0; i<arr.length; i++) {
-			if(element == arr[i]) {
-				System.out.println("Element found in "+i+"th position");
+			if(arr[i] == element) {
+				System.out.println("The element is present at "+i+"th positon");
 				count++;
 				break;
 			}
 		}
 		if(count == 0) {
-			System.out.println("Element not found");
+			System.out.println("The element is not found");
 		}
 	}
 
 	//driver code
 	public static void main(String args[]) {
-		//now let's take an user input
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the array size");
 		int size = sc.nextInt();
-		int arr[] = new int[size];
 		System.out.println("Enter the array elements");
+		int arr[] = new int[size];
 		for(int i=0; i<arr.length; i++) {
 			arr[i] = sc.nextInt();
 		}
 		System.out.println("Array is "+Arrays.toString(arr));
-		
-		//ask user for search input
 		System.out.println("Enter the element to search");
 		int element = sc.nextInt();
-		//now let's call the method for searching
+		
+		//call the method for linear search
 		linearSearch(arr, element);
 	}
 }
